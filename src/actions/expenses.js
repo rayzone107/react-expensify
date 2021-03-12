a0e23cec25a95e131064607c900812a3a86432a8
Rachit Goyal <rachitgoy@gmail.com>
2021-03-12 19:30:04 +0530

    Setup start remove expense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -34,6 +34,15 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`)
+            .remove().then(() => {
+                dispatch(removeExpense({ id }));
+            });
+    };
+}
+
 // EDIT_EXPENSE
 export const editExpense = (id, updates) => ({
     type: "EDIT_EXPENSE",
@@ -58,7 +67,6 @@ export const startSetExpense = (expenseData = {}) => {
                         ...childSnapshot.val()
                     });
                 });
-                console.log(expenses);
                 dispatch(setExpenses(expenses));
             });
     };
